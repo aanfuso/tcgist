59edf89e7052cedfde08c326f779e960021b5348
Agustin Anfuso <anfusoagustin89@gmail.com>
2023-08-23 11:23:47 +0400

    Show basic Layout and Buylist page

---
+++ src/pages/Buylist/index.js
@@ -0,0 +1,18 @@
+import { Container } from '@mui/material'
+
+const sectionStyles = {
+  height: '100vh',
+  display: 'flex',
+}
+
+function Buylist() {
+  return (
+    <Container
+      id="whatsapp-buylist"
+      sx={sectionStyles}
+    >
+    </Container>
+  )
+}
+
+export default Buylist
