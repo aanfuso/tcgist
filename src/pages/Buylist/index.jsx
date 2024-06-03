368922f3d8e3bb5d32c0b0bd95da0904f62e54db
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-06-03 16:41:52 +0400

    Add Layout back

--- src/pages/Buylist/index.jsx
+++ src/pages/Buylist/index.jsx
@@ -4,6 +4,8 @@ import { Box, Divider, Grid, List, Typography } from "@mui/material";
 import { BuylistItem } from "./BuylistItem";
 import { CardPreview } from "./CardPreview";
 
+import Layout from "lib/components/Layout";
+
 import { SAMPLE_BUYLIST } from "./data";
 
 const sectionStyles = { p: 10 };
@@ -31,34 +33,36 @@ export function Buylist() {
   ));
 
   return (
-    <Box id="whatsapp-buylist" sx={sectionStyles}>
-      <Typography gutterBottom variant="h5">
-        {name}'s buylist
-      </Typography>
+    <Layout>
+      <Box id="whatsapp-buylist" sx={sectionStyles}>
+        <Typography gutterBottom variant="h5">
+          {name}'s buylist
+        </Typography>
 
-      <Typography gutterBottom variant="body1">
-        {`This list has ${items.length} individual items`}
-      </Typography>
+        <Typography gutterBottom variant="body1">
+          {`This list has ${items.length} individual items`}
+        </Typography>
 
-      <Typography gutterBottom variant="body1">
-        {`This list has ${items.reduce((n, {quantity}) => n + quantity, 0)} items in total`}
-      </Typography>
+        <Typography gutterBottom variant="body1">
+          {`This list has ${items.reduce((n, {quantity}) => n + quantity, 0)} items in total`}
+        </Typography>
 
-      <Divider />
+        <Divider />
 
-      <Grid container>
-        <Grid item sm={2} md={3} sx={{ display: { xs: "none", md: "block" } }}>
-          <CardPreview item={preview} />
+        <Grid container>
+          <Grid item sm={2} md={3} sx={{ display: { xs: "none", md: "block" } }}>
+            <CardPreview item={preview} />
+          </Grid>
+
+          <Grid item xs={12} md={9}>
+            <Box sx={{ display: "flex", flexFlow: "column wrap" }}>
+              <List sx={{ columnCount: { sm: 1, md: 2, lg: 2, xl: 3 } }}>
+                {listing}
+              </List>
+            </Box>
+          </Grid>
         </Grid>
-
-        <Grid item xs={12} md={9}>
-          <Box sx={{ display: "flex", flexFlow: "column wrap" }}>
-            <List sx={{ columnCount: { sm: 1, md: 2, lg: 2, xl: 3 } }}>
-              {listing}
-            </List>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Layout>
   );
 };
