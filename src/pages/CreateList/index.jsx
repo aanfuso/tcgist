6d3e212110c1fe9c7da7c9b4bc4c49cd39572cd7
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-06-11 17:43:53 +0400

    First stab at list importer

---
+++ src/pages/CreateList/index.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { Button, Container, Input, Stack, Typography } from '@mui/material';
+
+import Layout from 'lib/components/Layout';
+
+import {
+  parseLine,
+  convertToCSV,
+  loadFromLocalStorage,
+ } from './utils';
+
+const SECTION_STYLES = {
+  pt: 10,
+  height: "100vh",
+};
+
+export function CreateList() {
+  const [list, setList] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const stored = loadFromLocalStorage();
+
+    if (stored.length) {
+      const toText = convertToCSV(stored);
+
+      console.log(toText);
+      setList(toText);
+    }
+  }, []);
+
+  useEffect(() => {
+    setData(list.split('\n').map(parseLine));
+  }, [list]);
+
+  const handleChange = (event) => {
+    const text = event.target.value;
+
+    setList(text);
+  }
+
+  const handleSave = () => {
+    console.log(data);
+
+    localStorage.setItem("buylist", JSON.stringify(data));
+
+    window.location.href = "/";
+  }
+
+  return (
+    <Layout>
+      <Container sx={SECTION_STYLES}>
+        <Typography gutterBottom variant="h5">
+          Create a new list
+        </Typography>
+          <Input
+            aria-label="List input"
+            multiline
+            placeholder="Paste your list here..."
+            onChange={handleChange}
+            value={list}
+          />
+
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={2}
+        >
+          <Button
+            variant="contained"
+            onClick={handleSave}
+          >
+            Save
+          </Button>
+        </Stack>
+      </Container>
+    </Layout>
+  );
+};
---
+++ src/pages/CreateList/utils.js
@@ -0,0 +1,54 @@
+export const convertToCSV = (arr) => {
+  return arr.map(it => {
+    return it.line
+  }).join('\n')
+};
+
+
+export const loadFromLocalStorage = () => {
+  const buylist = localStorage.getItem("buylist");
+
+  if (buylist) {
+    return JSON.parse(buylist);
+  }
+
+  return [];
+};
+
+const parseByApp = {
+  mox: {
+    setIndex: ["(", ")"],
+    nameIndex: [" ", " ("],
+  },
+  tcg: {
+    setIndex: ["[", "]"],
+    nameIndex: [" ", " ["],
+    variantIndex: ["(", ")"],
+  },
+};
+
+export const parseLine = (line) => {
+  const app = "tcg";
+  const { setIndex, nameIndex, variantIndex } = parseByApp[app];
+  const quantity = line.substring(0, line.indexOf(" "));
+  const set = line.substring(
+    line.indexOf(setIndex[0]) + 1,
+    line.indexOf(setIndex[1])
+  );
+  const name = line.substring(
+    line.indexOf(nameIndex[0]) + 1,
+    line.indexOf(nameIndex[1])
+  );
+  const variant = line.substring(
+    line.indexOf(variantIndex[0]) + 1,
+    line.indexOf(variantIndex[1])
+  );
+
+  return {
+    quantity,
+    name,
+    set,
+    variant,
+    line,
+  };
+};
