6a82008e5309b904960c28d097c5b5aa1f753ed6
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-07-19 15:15:42 +0400

    Linting

--- src/lib/firebase/index.js
+++ src/lib/firebase/index.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
@@ -9,5 +9,5 @@ const firebaseConfig = {
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FIREBASE_CONFIG);
 export const db = getDatabase(app);
