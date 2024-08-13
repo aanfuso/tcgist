70f9e8d17cdc814ed23ed65c258fd76e7964d192
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-08-13 20:54:30 +0400

    Limit share message to 10 items

--- src/pages/Collection/SetCompletion/index.jsx
+++ src/pages/Collection/SetCompletion/index.jsx
@@ -20,7 +20,9 @@ export default function SetCompletion(props) {
   const cards = rows?.filter((row) => missing.includes(row.collectorNumber));
   const handleMessage = () => {
     const list = cards
+      .sort((a, b) => a.collectorNumber - b.collectorNumber)
       .map((card) => `- ${card.name} #${card.collectorNumber} ($${card.price})`)
+      .slice(0, 10)
       .join('\n');
     const message = `I'm looking for these cards from Dominaria United: \n\n${list}\n\nWould you check if you have any of them? \nVisual spoiler: https://tcgist.pages.dev/agu/mtg/dmu`;
     const url = `whatsapp://send?text=${encodeURIComponent(message)}`;
