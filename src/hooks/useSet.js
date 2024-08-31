f975ff00d3d609664d5bf6d1b90938a85681c942
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-08-31 12:50:02 +0400

    Move set request to requests

--- src/hooks/useSet.js
+++ src/hooks/useSet.js
@@ -1,16 +1,14 @@
 import { useState, useEffect } from 'react';
 import { parseCard } from 'pages/Collection/utils';
 
-import { getFullSet } from 'requests';
+import { getSet, getFullSet } from 'requests';
 
 export default function useSet(setCode) {
   const [set, setSet] = useState({});
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    fetch(`https://api.scryfall.com/sets/${setCode}`)
-      .then((response) => response.json())
-      .then((data) => setSet(data));
+    getSet(setCode).then((data) => setSet(data));
 
     getFullSet(setCode)
       .then(({ data }) => data.map(parseCard))
--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,14 @@
 
 const SEARCH_URL = 'https://api.scryfall.com/cards/search';
 
-const getSet = async (setCode, page = 1) => {
+export const getSet = async (setCode) => {
+  const response = await fetch(`https://api.scryfall.com/sets/${setCode}`);
+  const data = await response.json();
+
+  return data;
+};
+
+const getSetCards = async (setCode, page = 1) => {
   const response = await fetch(`${SEARCH_URL}?page=${page}&order=set&q=%28game%3Apaper%29+set%3A${setCode}`);
   const data = await response.json();
 
@@ -9,7 +16,7 @@ const getSet = async (setCode, page = 1) => {
 };
 
 export const getFullSet = async (setCode, page = 1) => {
-  const response = await getSet(setCode, page);
+  const response = await getSetCards(setCode, page);
 
   if (response.has_more) {
     const more = await getFullSet(setCode, page + 1);
